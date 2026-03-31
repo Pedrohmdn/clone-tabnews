fcf9d0b6112b1b10e56433febbf77fe1011f3f74
Pedro Henrique <pedro.hmacedo2@gmail.com>
2026-03-31 19:45:32 -0300

    imagem adicionada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Hello World!🤘^_^🤘</h1>;
+  return (
+    <div>
+      <h1>Hello World!🤘^_^🤘</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif" alt="Your Image" />
+    </div>
+  );
 }
 
 export default Home;
